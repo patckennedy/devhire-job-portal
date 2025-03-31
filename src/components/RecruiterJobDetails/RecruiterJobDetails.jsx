f89f227a45ee97a98c9cc3f8c856abebb03b53da
patriciakennedy <42810547+patriciakennedy@users.noreply.github.com>
2025-03-30 19:29:02 -0500

    feat: add initial pages and views for DevHire

---
+++ src/components/RecruiterJobDetails/RecruiterJobDetails.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const RecruiterJobDetails = () => {
+    return <div>RecruiterJobDetails</div>;
+};
+
+export default RecruiterJobDetails;
---
+++ src/components/ViewJobDetails/ViewJobDetails.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ViewJobDetails = () => {
+    return <div>ViewJobDetails</div>;
+};
+
+export default ViewJobDetails;
