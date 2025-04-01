b68e0e30f9b7977623ffb0bf89ff3c049c189f2c
patriciakennedy <42810547+patriciakennedy@users.noreply.github.com>
2025-03-31 20:30:25 -0500

    Update App.jsx and Nav.jsx to test routing

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -6,35 +6,40 @@ function Nav() {
   const user = useStore((store) => store.user);
 
   return (
-    <nav>
-      <ul>
-      { // User is not logged in, render these links:
-        !user.id && (
-          <>
-            <li>
-              <NavLink to="/login">Login</NavLink>
-            </li>
-            <li>
-              <NavLink to="/registration">Register</NavLink>
-            </li>
-          </>
-        )
-      }
-      { // User is logged in, render these links:
-        user.id && (
-          <>
-            <li>
-              <NavLink to="/">Home</NavLink>
-            </li>
-          </>
-        )
-      }
-      {/* Show these links regardless of auth status: */}
-        <li>
-          <NavLink to="/about">About</NavLink>
-        </li>
-      </ul>
-    </nav>
+      <nav>
+          <ul>
+              {
+                  // User is not logged in, render these links:
+                  !user.id && (
+                      <>
+                          <li>
+                              <NavLink to="/login">Login</NavLink>
+                          </li>
+                          <li>
+                              <NavLink to="/registration">Register</NavLink>
+                          </li>
+                      </>
+                  )
+              }
+              {
+                  // User is logged in, render these links:
+                  user.id && (
+                      <>
+                          <li>
+                              <NavLink to="/">Home</NavLink>
+                          </li>
+                      </>
+                  )
+              }
+              {/* Show these links regardless of auth status: */}
+              <li>
+                  <NavLink to="/about">About</NavLink>
+              </li>
+              {/* <li>
+                  <NavLink to="/job-listing">Jobs</NavLink>
+              </li> */}
+          </ul>
+      </nav>
   );
 }
 
