1b6c463f0fd7229d847c88088538155dada2bf74
patriciakennedy <42810547+patriciakennedy@users.noreply.github.com>
2025-05-10 08:42:03 -0500

    chore: update stores.js with latest state management changes

--- src/zustand/store.js
+++ src/zustand/store.js
@@ -1,11 +1,17 @@
-import { create } from "zustand";
+import { create } from 'zustand';
 import userSlice from './slices/user.slice.js';
 
-
 // Combine all slices in the store:
-const useStore = create((...args) => ({
-  ...userSlice(...args),
-}))
+const useStore = create((...args) => {
+    const store = {
+        ...userSlice(...args),
+    };
 
+    if (typeof window !== 'undefined') {
+        window.__STORE__ = store;
+    }
+
+    return store;
+});
 
 export default useStore;
